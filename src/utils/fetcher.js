f0bc1c8a14b401add6f42ab77ca82943e20fc33b
CH Hsu <zarose0000ds@gmail.com>
2025-08-05 10:01:52 +0800

    chore: add retry to fetcher, modifying test page [sc-80043]

--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -1,21 +1,25 @@
 const fetcher = {
   async get(url = '', init = {}) {
-    try {
-      const res = await fetch(url, {
-        method: 'GET',
-        ...init
-      })
-      if (res.status === 200) {
-        const contentType = res.headers.get('Content-Type')
-        if (contentType.includes('application/json')) {
-          const result = await res.json()
-          return result
+    let maxTry = 5
+    do {
+      maxTry--
+      console.log(`Fetching: ${url} (remaining tries: ${maxTry})`)
+      try {
+        const res = await fetch(url, {
+          method: 'GET',
+          ...init
+        })
+        if (res.status === 200) {
+          const contentType = res.headers.get('Content-Type')
+          if (contentType.includes('application/json')) {
+            const result = await res.json()
+            return result
+          }
         }
-
+      } catch (err) {
+        console.error(err)
       }
-    } catch (err) {
-      console.error(err)
-    }
+    } while (maxTry)
     return null
   }
 }
