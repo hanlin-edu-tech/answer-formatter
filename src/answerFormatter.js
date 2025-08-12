6d43d16bfaa0538600a06ec0919c596d7e6a2b98
CH Hsu <zarose0000ds@gmail.com>
2025-08-12 10:59:38 +0800

    fix: modify export way to avoid fetching match table duplicately

--- src/answerFormatter.js
+++ src/answerFormatter.js
@@ -5,39 +5,31 @@ const defaultTable = require('./data/matchTable.json')
 
 const { MODE, VERSION, API_NAMESPACE } = config.getConfig()
 
-const buildAnswerFormatter = () => {
-	const answerFormatter = {
-		mode: MODE,
-		version: VERSION,
-		matchTable: defaultTable,
-		format(answer) {
-			let result = answer
-			for (let i = 0; i < formatters.length; i++) {
-				result = formatters[i](result, answerFormatter)
-			}
-			return result
-		},
-		equals(answer1, answer2) {
-			return answerFormatter.format(answer1) == answerFormatter.format(answer2)
+const answerFormatter = {
+	mode: MODE,
+	version: VERSION,
+	matchTable: defaultTable,
+	format(answer) {
+		let result = answer
+		for (let i = 0; i < formatters.length; i++) {
+			result = formatters[i](result, answerFormatter)
 		}
+		return result
+	},
+	equals(answer1, answer2) {
+		return answerFormatter.format(answer1) == answerFormatter.format(answer2)
 	}
-
-	new Promise(async (resolve) => {
-		answerFormatter.matchTable = await api.getMatchTable() || defaultTable
-		console.log('answer formatter fetched match table')
-		resolve()
-	})
-
-	if (typeof window !== 'undefined' && window) {
-		window[API_NAMESPACE] = answerFormatter
-	}
-
-	return answerFormatter
 }
 
-if (typeof module !== 'undefined' && module.exports) {
-	module.exports = buildAnswerFormatter()
-}
+new Promise(async (resolve) => {
+	answerFormatter.matchTable = await api.getMatchTable() || defaultTable
+	console.log('answer formatter fetched match table')
+	resolve()
+})
+
 if (typeof window !== 'undefined' && window) {
-	buildAnswerFormatter()
+	window[API_NAMESPACE] = answerFormatter
+}
+if (typeof module !== 'undefined' && module.exports) {
+	module.exports = answerFormatter
 }
