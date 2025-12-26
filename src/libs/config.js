f012818ae9986f4710a2b7ed9b4d70e95675fec1
CH Hsu <zarose0000ds@gmail.com>
2025-12-26 09:54:29 +0800

    chore: modify config setting

--- src/libs/config.js
+++ src/libs/config.js
@@ -4,7 +4,6 @@ const generalConfig = {
   VERSION: process.env.VERSION || 'none',
   API_NAMESPACE: 'answerFormatter',
   MATCH_TABLE_PATH: 'v1/api/answerFormatter/matchTable.json',
-  LLM_PROXY_ENDPOINT: '/' // Default for prod, assuming same domain
 }
 
 const AWS_S3_BUCKET_TEST = process.env.AWS_S3_BUCKET_TEST || 'tw-itembank-sandbox'
@@ -12,16 +11,11 @@ const AWS_S3_REGION_TEST = process.env.AWS_S3_REGION_TEST || 'ap-east-2'
 const AWS_S3_BUCKET_PROD = process.env.AWS_S3_BUCKET_PROD || 'itembank'
 const AWS_S3_REGION_PROD = process.env.AWS_S3_REGION_PROD || 'ap-southeast-1'
 
-const devConfig = {
-  MODE: mode.toUpperCase(),
-  ITEMBANK_ITEM_S3_ENDPOINT: `https://${AWS_S3_BUCKET_TEST}.s3.${AWS_S3_REGION_TEST}.amazonaws.com`,
-  ITEMBANK_ITEM_CLOUDFRONT_ENDPOINT: `https://item.ehanlin.com.tw`,
-  BACKEND_ENDPOINT: 'http://localhost:8080'
-}
 const testConfig = {
   MODE: mode.toUpperCase(),
   ITEMBANK_ITEM_S3_ENDPOINT: `https://${AWS_S3_BUCKET_TEST}.s3.${AWS_S3_REGION_TEST}.amazonaws.com`,
   ITEMBANK_ITEM_CLOUDFRONT_ENDPOINT: `https://item.ehanlin.com.tw`,
+  // BACKEND_ENDPOINT: 'http://localhost:8080',
   BACKEND_ENDPOINT: 'https://answer-formatter-script-184800465453.asia-east1.run.app'
 }
 const prodConfig = {
@@ -31,7 +25,7 @@ const prodConfig = {
   BACKEND_ENDPOINT: 'https://answer-formatter-script-613393819622.asia-east1.run.app'
 }
 const configMap = {
-  dev: devConfig,
+  dev: testConfig,
   test: testConfig,
   prod: prodConfig
 }
